06e3f0dc841ded0241929c3eb7e8e216203d3a57
nnastaran83 <nnastaran83@gmail.com>
2023-03-17 15:44:14 +0200

    fixed styling + added video call from facetime

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
 
     return (
         <div className="App">
-            {/*
+
             <div className="ui center aligned container">
 
                 <div className="ui basic center aligned segment">
@@ -59,14 +59,17 @@ const App = () => {
             </div>
             <BottomMenu handleHomeButtonClick={handleHomeButtonClick}
                         handleVideoCallButtonClick={handleVideoCallButtonClick}/>
-*/}
-            return (
-            <div className="App">
+
+            {/*
+                return (
+                <div className="App">
                 <h1>Counter</h1>
                 <p>Count: {count}</p>
                 <button onClick={() => setCount(count + 1)}>Increment</button>
             </div>
             );
+            */}
+
         </div>
 
 
