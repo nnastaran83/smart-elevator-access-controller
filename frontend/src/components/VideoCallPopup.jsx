f602710b53ec5424e9c6301aa8bf69b1c33b6e3e
nnastaran83 <nnastaran83@gmail.com>
2023-04-17 19:56:03 +0300

    added video call popup

---
+++ frontend/src/components/VideoCallPopup.jsx
@@ -0,0 +1,22 @@
+import React, {useEffect, useState} from 'react';
+import '../styles/PopupFrameOverlay.css';
+
+/**
+ * VideoCallPopup component is used to display a frame for video call.
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const VideoCallPopup = () => {
+    return (
+        <div className="video-call-container">
+            <div className="overlay">
+                <div className="inner-popup">
+                    <iframe style={{width: "100%", height: "100%", overflowY: "hidden"}}
+                            src="http://localhost:5173/contactlist"/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default VideoCallPopup;
