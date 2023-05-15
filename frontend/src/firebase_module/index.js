efc269fbf46f885a76acfda9291e9a5a112359b1
nnastaran83 <nnastaran83@gmail.com>
2023-05-15 16:42:05 +0300

    working on message

--- frontend/src/firebase_module/index.js
+++ frontend/src/firebase_module/index.js
@@ -1,12 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {initializeApp} from "firebase/app";
+import {getFirestore, collection, getDocs} from "firebase/firestore";
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut,
 } from "firebase/auth";
+import {getAnalytics} from "firebase/analytics";
+import {getToken} from "firebase/messaging";
+import {getMessaging} from "firebase/messaging/sw";
 
 /**
  * Web Firebase configuration
@@ -14,18 +17,20 @@ import {
  * @see https://firebase.google.com/docs/web/setup#available-libraries
  * @type {{storageBucket: string, apiKey: string, messagingSenderId: string, appId: string, projectId: string, measurementId: string, authDomain: string}}
  */
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBWLr7DrODHLbB2Zvb5o6ANvBAws0MH8_8",
-  authDomain: "smart-4e774.firebaseapp.com",
-  projectId: "smart-4e774",
-  storageBucket: "smart-4e774.appspot.com",
-  messagingSenderId: "267084890357",
-  appId: "1:267084890357:web:6aa5d0b170c3dcb44f45bc",
-  measurementId: "G-KQTK3EWGST",
+    apiKey: "AIzaSyBWLr7DrODHLbB2Zvb5o6ANvBAws0MH8_8",
+    authDomain: "smart-4e774.firebaseapp.com",
+    projectId: "smart-4e774",
+    storageBucket: "smart-4e774.appspot.com",
+    messagingSenderId: "267084890357",
+    appId: "1:267084890357:web:73c058772bfe998b4f45bc",
+    measurementId: "G-XLLC8D755N",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
@@ -33,11 +38,37 @@ const db = getFirestore(app);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-// To apply the default browser preference instead of explicitly setting it.
-/**
- * Sign up the user
- * @param email
- * @param password
- */
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = getMessaging(app);
 
-export { db, auth };
+
+/**
+ * Get registration token. Initially this makes a network call, once retrieved
+ * subsequent calls to getToken will return from cache.
+ */
+const getMessagingToken = () => {
+    getToken(messaging, {
+        vapidKey:
+            "BEoYSAt84uere2NDQf_nu4DE-3FhTCyJHgqtMWATFfdbrz-nxneYlwKR9SeENhJ1ZBwWCTA1Oq4MVdpjERk-cKs",
+    })
+        .then((currentToken) => {
+            if (currentToken) {
+                // Send the token to your server and update the UI if necessary
+                console.log(currentToken);
+            } else {
+                // Show permission request UI
+                console.log(
+                    "No registration token available. Request permission to generate one."
+                );
+                // ...
+            }
+        })
+        .catch((err) => {
+            console.log("An error occurred while retrieving token. ", err);
+            // ...
+        });
+
+};
+
+
+export {db, auth, messaging};
